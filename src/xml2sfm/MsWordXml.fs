2e0339fab7651978e49e7824a4ad9efe62b51bce
Kazark <kazark@zoho.com>
2015-07-03 15:07:38 -0400

    First time ever using an XML type provider!
    After a year of using F#, glad to finally experience what all the hype
    is about for myself. This is certainly a superior way of working with
    XML---honestly, it's always what I've _wanted_ to do with XML and never
    could.
    
    @rachelreese, I think you were the first to show me these, at CodeMash
    2015. Thank you! @ovatsus and @tpetricek I believe I owe you a thank-you
    as well. Rock on, F# folks!
    
    @mjgpy3 Have you heard of F# type providers? Get on the bus, man!

---
+++ src/xml2sfm/MsWordXml.fs
@@ -0,0 +1,33 @@
+﻿namespace xml2sfm
+
+[<RequireQualifiedAccess>]
+module MsWordXml =
+    open System.IO
+    open FSharp.Data
+
+    type Parser = XmlProvider<"./sample.xml">
+
+    [<RequireQualifiedAccess>]
+    type Parse =
+        | Failed
+        | Succeeded of string
+
+    let parse xml =
+        let parsedXml = Parser.Parse xml
+        let parts = parsedXml.Parts
+        let document =
+            parsedXml.Parts
+            |> Seq.filter (fun part -> part.Name = "/word/document.xml")
+            |> Seq.map (fun part -> part.XmlData.Document)
+            |> Seq.exactlyOne
+        if document.IsNone
+        then Parse.Failed
+        else
+            let body = document.Value.Body
+            body.Ps
+            |> Seq.map (fun p ->
+                p.OMaths
+                |> Seq.map (fun oMath -> oMath.M.Mr.E.R.T))
+            |> Seq.concat
+            |> System.String.Concat
+            |> Parse.Succeeded
---
+++ src/xml2sfm/Program.fs
@@ -0,0 +1,34 @@
+﻿namespace xml2sfm
+
+module EntryPoint =
+    open System.IO
+    open System.Text
+
+    let transform (inputPath : string) outputPath =
+        let xml = File.ReadAllText(inputPath, Encoding.UTF8)
+        match MsWordXml.parse xml with
+        | MsWordXml.Parse.Succeeded text ->
+            printfn "Experiment: %s" text
+        | MsWordXml.Parse.Failed ->
+            printfn "Parse failed!"
+
+    let usage() =
+        printfn "Usage: xml2sfm inputPath [outputPath]"
+
+    [<EntryPoint>]
+    let main argv = 
+        match argv with
+        | [||] -> 
+            printfn "Error: Too few command-line arguments."
+            usage()
+            1
+        | [|inputPath|] ->
+            transform inputPath "output.sfm"
+            0
+        | [|inputPath; outputPath|] ->
+            transform inputPath outputPath
+            0
+        | _ ->
+            printfn "Error: Too many command-line arguments."
+            usage()
+            2
